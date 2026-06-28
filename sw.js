ad42caa26c8ba291c746287e9da53e2c19c69761
KutayAkalin <kutay.akalin.95@gmail.com>
2026-06-28 21:19:45 +0200

    update trainer 2026-06-28

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='lena-trainer-20260627-224026';
+const CACHE='lena-trainer-20260628-211945';
 const ASSETS=['trainer.html','manifest.webmanifest','icon.svg'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
